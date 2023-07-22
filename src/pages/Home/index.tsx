d11627b04efb70b9442420e2452cc58d1b9125e3
Enzo Bozzani <bozzanienzo@gmail.com>
2023-07-22 15:06:06 -0300

    feat: made all pages responsive and fixed some bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,26 @@ export const Home = () => {
                 <section>
                     <p className={styles.subtitle}><span>Recent Items</span><span>Actions</span></p>
                     <div>
-                        {recentItems.map(item => <span className={styles.item} key={item.id}><span>{item.name}</span><Link to={`/items/${item.id}`}><button>Ver</button></Link></span>)}
+                        {recentItems.length !== 0 ?
+                            recentItems.map(item => <span className={styles.item} key={item.id}><span>{item.name}</span><Link to={`/items/${item.id}`}><button>Ver</button></Link></span>)
+                            :
+                            (
+                                <h2>There are no recent items!</h2>
+                            )
+                        }
+
                     </div>
                 </section>
                 <section>
                     <p className={styles.subtitle}><span>Items Running Out</span><span>Quantity</span><span>Actions</span></p>
                     <div>
-                        {runningOut.map(item => <span className={styles.item} key={item.id}><span>{item.name}</span><span>{item.qtd}</span><Link to={`/items/${item.id}`}><button>Ver</button></Link></span>)}
+                        {runningOut.length !== 0 ?
+                            runningOut.map(item => <span className={styles.item} key={item.id}><span>{item.name}</span><span>{item.qtd}</span><Link to={`/items/${item.id}`}><button>Ver</button></Link></span>)
+                            :
+                            (
+                                <h2>There are no running out items!</h2>
+                            )
+                        }
                     </div>
                 </section>
             </section>
--- src/pages/Items/index.tsx
+++ src/pages/Items/index.tsx
@@ -18,28 +18,34 @@ export const Items = () => {
                     <span>Category</span>
                     <span>Actions</span>
                 </span>
-                {items.map((item: any) => {
-                    return (
-                        <span className={styles.item} key={item.id}>
-                            <span>{item.id.slice(0, 15)}...</span>
-                            <span>{item.name}</span>
-                            <span>{item.qtd} items</span>
-                            <span>{item.category}</span>
-                            <span className={styles.btns}>
-                                <Link to={`/items/${item.id}`}><button className={styles.seeBtn}>See</button></Link>
-                                <Link to={`/items/edit/${item.id}`}><button className={styles.editBtn}>Edit</button></Link>
-                                <button className={styles.deleteBtn} onClick={() => {
-                                    localStorage.removeItem(item.id);
-                                    setItems((currentState: any) => {
-                                        return currentState.filter((i: any) => i !== item)
-                                    })
-                                }}>
-                                    Delete
-                                </button>
+                {items.length !== 0 ?
+                    items.map((item: any) => {
+                        return (
+                            <span className={styles.item} key={item.id}>
+                                <span>{item.id.slice(0, 15)}...</span>
+                                <span>{item.name}</span>
+                                <span>{item.qtd} items</span>
+                                <span>{item.category}</span>
+                                <span className={styles.btns}>
+                                    <Link to={`/items/${item.id}`}><button className={styles.seeBtn}>See</button></Link>
+                                    <Link to={`/items/edit/${item.id}`}><button className={styles.editBtn}>Edit</button></Link>
+                                    <button className={styles.deleteBtn} onClick={() => {
+                                        localStorage.removeItem(item.id);
+                                        setItems((currentState: any) => {
+                                            return currentState.filter((i: any) => i !== item)
+                                        })
+                                    }}>
+                                        Delete
+                                    </button>
+                                </span>
                             </span>
-                        </span>
+                        )
+                    })
+                    :
+                    (
+                        <h2>There are no items in stock!</h2>
                     )
-                })}
+                }
             </main>
         </>
     )
